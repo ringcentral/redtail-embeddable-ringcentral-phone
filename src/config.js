268efd8a0544af3c687bfc1f07389f7d6c7cb044
ZHAO Xudong <zxdong@gmail.com>
2019-02-01 09:14:04 +0800

    Add sms support

--- src/config.js
+++ src/config.js
@@ -54,61 +54,7 @@ function getIds(href) {
 
 
 export const insertClickToCallButton = [
-  {
-    // must match page url
-    shouldAct: href => {
-      return /\/contacts\/\d+/.test(href)
-    },
 
-    // define in the page how to get phone number,
-    // if can not get phone number, will not insert the call button
-    getContactPhoneNumbers: async () => {
-      let sel = '.contact-phones .number'
-      let doms = Array.from(
-        document.querySelectorAll(sel)
-      )
-      return doms.reduce((prev, dom, i) => {
-        let txt = dom.textContent.trim().replace(':', '')
-        if (!checkPhoneNumber(txt)) {
-          return prev
-        }
-        let title = dom.previousElementSibling.textContent.trim()
-        return [
-          ...prev,
-          {
-            id: i + 'pn',
-            title,
-            number: txt
-          }
-        ]
-      }, [])
-    },
-
-    // parent dom to insert call button
-    // can be multiple condition
-    // the first one matches, rest the array will be ignored
-    parentsToInsertButton: [
-      {
-        getElem: () => {
-          let dom = document.querySelector('#contact-masthead2 .rc-btn-wrapper')
-          if (!dom) {
-            let wrap = createElementFromHTML(`
-              <div class="rc-btn-wrapper"></div>
-            `)
-            let parent = document.querySelector('#contact-masthead2')
-            parent.parentNode.classList.add('rc-wrapped')
-            parent.insertBefore(wrap, document.querySelector('#contact-masthead2 > .col-sm-12'))
-            return wrap
-          }
-          return dom
-        },
-        insertMethod: 'insertBefore',
-        shouldInsert: () => {
-          return !document.querySelector('.' + RCBTNCLS2)
-        }
-      }
-    ]
-  }
 ]
 
 //hover contact node to show click to dial tooltip
